37a48346b1ab5998f066fd9dc35441cd99468f79
ViktoriiaIlzenetsa <mazabubochka@gmail.com>
2024-03-12 16:42:00 +0200

    first commit

---
+++ create_fake_files.py
@@ -0,0 +1,23 @@
+import logging
+from faker import Faker
+from os import path, mkdir
+
+
+FILES_COUNT = 60
+
+for i in range(3):
+    parent_dir = path.relpath(f"{i}")
+    mkdir(parent_dir)
+
+
+fake = Faker()
+for i in range(FILES_COUNT):
+    file_name = fake.word() + ".txt"
+    folder = f"{i%3}"
+    file_path = path.relpath(f"{folder}/{file_name}")
+    with open(file_path, "a") as f:
+        for _ in range(100):
+            f.write(fake.text())
+
+
+
